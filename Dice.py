c6db6a891611e7612c1a0277ce0ed3b7bf7e2d2a
Prnotideo <pranoti@qtip.me>
2019-10-01 13:37:45 +0300

    Program is modified using Class

--- Dice.py
+++ Dice.py
@@ -1,45 +1,65 @@
 import sys
 import random
-
-def Player():
-    if N > 6:
-        print("Players should be less than 6")
-    elif N < 2:
-        print("Players should be greater than 2")
-    else:
-        list = []
-        for i in range(N):
-            s = str(input("Enter Name: "))
-            list.append(s)
-        list.sort()
-        print(list)
+N = int(input("Enter Number of players: "))
+class dice(object):
+    def Roll(self):
+        Opt = input("Roll Exit Pass ")
 
         for i in range(N):
-            print("It's your turn: "+list[i])
-            Opt = input("Roll Exit Pass ")
-            list2 = []
             if Opt == "Exit":
                 print("Ok")
             elif Opt == "Pass":
                 print(Score)
             elif Opt == "Roll":
                 Score = random.randrange(1,7)
+                print(Score)
                 if Score == 6:
                     Score1 = random.randrange(1,7)
                     Score = Score1+Score
+                    print(Score)
                     if Score1 == 6:
                         Score2 = random.randrange(1,7)
                         Score = Score2+Score
+                        print(Score)
                         if Score2 == 6:
-                            Score = 0
-                print(list[i],Score)
-                list2.append(Score)
-    dict1 = dict.fromkeys(list[i],list2[i])
-    print(dict1)
+                                Score = 0
+                                print(Score)
+                list2 = []
+                for j in range(6):
+                    list2[j] = Score
+            print(list2)
+
+class Player(object):
+
+    global N
+
+    def add_players():
+        if N > 7:
+            print("Enter maximum 6 players")
+        elif N < 2:
+            print("Enter minimum 2 players")
+        else:
+            list = []
+            for i in range(N):
+                s = str(input("Enter Name: "))
+                list.append(s)
+            list.sort()
+            print(list)
+
+
+            for j in range(N):
+                print("It's your turn: "+list[j])
+                a = dice()
+                a.Roll()
+
+    add_players()
 
 
 
-N = int(input("Enter Number of players: "))
 
-Player()
-#M = str(input("Enter their Names: "))
+
+
+
+
+    #dict1 = dict.fromkeys(list, Score)
+    #print(dict1)
