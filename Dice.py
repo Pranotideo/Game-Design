26a933423a853fcf905bbbc0fe040498b8d6d5de
Prnotideo <pranoti@qtip.me>
2019-10-09 13:10:42 +0300

    latest program

--- Dice.py
+++ Dice.py
@@ -1,46 +1,53 @@
 import random
 N = int(input("Enter Number of players: "))
-class dice(object):
+class dice:
+    def opt_pass():
+        print(score)
+
+
+    def exit():
+        import sys
+        sys.exit()
+
     def Roll(self):
-        Opt = input("Roll Exit Pass ")
-
-        for i in range(N):
-            if Opt == "Exit":
-                print("Ok")
-            elif Opt == "Pass":
+        Score = random.randrange(1,7)
+        print(Score)
+        if Score == 6:
+            Score1 = random.randrange(1,7)
+            Score = Score1+Score
+            print(Score)
+            if Score1 == 6:
+                Score2 = random.randrange(1,7)
+                Score = Score2+Score
                 print(Score)
-            elif Opt == "Roll":
-                Score = random.randrange(1,7)
-                print(Score)
-                if Score == 6:
-                    Score1 = random.randrange(1,7)
-                    Score = Score1+Score
+                if Score2 == 6:
+                    Score = 0
                     print(Score)
-                    if Score1 == 6:
-                        Score2 = random.randrange(1,7)
-                        Score = Score2+Score
-                        print(Score)
-                        if Score2 == 6:
-                                Score = 0
-                                print(Score)
                 list2 = []
-                for j in range(6):
-                    list2[j] = Score
-            print(list2)
+                #for j in range(6):
+                #    list2[j] = Score
+        #    print(list2)
+    def __init__(self, score, Opt):
+        Opt = raw_input("Roll Exit Pass ")
+        if Opt == Roll:
+            Roll()
+        elif Opt == Pass:
+            opt_pass(score)
+        elif Opt == exit:
+            exit()
 
-class Player(object):
-
-    global N
+class Player:
 
     def add_players():
-        if N > 7:
-            print("Enter maximum 6 players")
-        elif N < 2:
-            print("Enter minimum 2 players")
+
+        if N > 7 or N < 2:
+            print("Enter minimum 2 and maximum 6 players")
+        #elif N < 2:
+        #    print("Enter minimum 2 players")
         else:
             list = []
             for i in range(N):
-                s = str(input("Enter Name: "))
+                s = raw_input("Enter Name: ")
                 list.append(s)
             list.sort()
             print(list)
@@ -53,7 +60,7 @@ class Player(object):
 
     add_players()
 
-
+    
 
 
 
