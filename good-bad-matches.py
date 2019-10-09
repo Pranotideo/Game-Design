2d557ad5dccd4312a84107a26ce4bded4bb67d69
Prnotideo <pranoti@qtip.me>
2019-10-09 10:25:25 +0300

    Improve your guess is added and game will run 10 times

--- good-bad-matches.py
+++ good-bad-matches.py
@@ -19,23 +19,27 @@ def match_words(w1, w2):
     print "User's guess: %s" % w2
     G = 0
     B = 0
+
     if w1 == w2:
         print("correct")
     else:
         for i in range(len(word)):
-
             if w1[i] == w2[i]:
                 G += 1
-
             elif w1[i] in w2:
                 B += 1
+            elif G == 0 and B == 0:
+                print("Good Match: %s" % G)
+                print("Bad Match: %s" % B)
+                w2 = raw_input ("Improve your guess: ")
+
+    print("Good Match: %s" % G)
+    print("Bad Match: %s" % B)
 
 
-
-        print("Good Match: %s" % G)
-        print("Bad Match: %s" % B)
 
 if __name__ == "__main__":
-    word = get_random_word()
-    user = get_user_input()
-    match_words(word, user)
+    for j in range(10):
+        word = get_random_word()
+        user = get_user_input()
+        match_words(word, user)
