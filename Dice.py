d1b5848026bbd699c78c5e4cfc476f93e1a0610f
Prnotideo <pranoti@qtip.me>
2019-10-16 13:57:57 +0300

    Modified with give_option function

--- Dice.py
+++ Dice.py
@@ -1,13 +1,37 @@
 import random
 N = int(input("Enter Number of players: "))
+list = []
+
+class Player:
+    def __init__(self, Opt):
+        self.Opt = Opt
+
+    def add_players():
+
+        if N > 7 or N < 2:
+            print("Enter minimum 2 and maximum 6 players")
+
+        else:
+
+            for i in range(N):
+                s = raw_input("Enter Name: ")
+                list.append(s)
+            list.sort()
+            print(list)
+    add_players()
+
 class Dice:
 
-    def __init__(self, Roll, Exit, Pass):
+    def __init__(self, Roll, Exit, Pass, give_option):
+        Player.add_players(self)
         self.Roll = Roll()
         self.Exit = Exit()
         self.Pass = Pass()
+        self.score_list = []
+        self.give_option = give_option()
 
     def Roll(self):
+
         score = random.randint(1, 7)
         print(score)
         if score == 6:
@@ -19,41 +43,33 @@ class Dice:
                 score = score2+score
                 print(score)
                 if score2 == 6:
-                    Score = 0
+                    score = 0
                     print(score)
+        self.score_list.append(score)
+        print(self.score_list)
         return score
+
     def Pass(self):
         return 0
+
     def Exit(self):
         return 0
 
+    def give_option():
+        for j in range(N):
+            print("It's your turn: "+list[j])
+            Opt = raw_input("Roll Exit Pass")
+            if Opt == "Roll":
+                Roll()
+            elif Opt == "Exit":
+                Exit()
+            elif Opt == "Pass":
+                Pass()
+            else:
+                print("Enter valid option ")
 
-class Player(Dice):
+    give_option()
 
-    def add_players():
-
-        if N > 7 or N < 2:
-            print("Enter minimum 2 and maximum 6 players")
-        #elif N < 2:
-        #    print("Enter minimum 2 players")
-        else:
-            list = []
-            for i in range(N):
-                s = raw_input("Enter Name: ")
-                list.append(s)
-            list.sort()
-            print(list)
-
-            for j in range(N):
-                print("It's your turn: "+list[j])
-                Opt = raw_input("Roll Exit Pass")
-                if Opt == "Roll":
-                    Dice(Roll, Exit, Pass)
-                elif Opt == "Exit":
-                    Dice.Exit()
-                elif Opt == "Pass":
-                    Dice.Pass()
-    add_players()
 
 
 
