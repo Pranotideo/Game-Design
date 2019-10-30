6145cab9e4ae3825ba27f923e83d5c37c9802ae0
Prnotideo <pranoti@qtip.me>
2019-10-30 13:21:01 +0200

    latest modified file

--- Dice.py
+++ Dice.py
@@ -1,98 +1,95 @@
-import random
-
-N = int(input("Enter Number of players: "))
-list1 = []
-score_list = []
-final_score = []
-count = 0
-score = 0
-
-
 class Player:
+
+    def __init__(self):
+        self.players_list = []
+        self.score_list = []
+        self.final_score = []
+        self.N = int(input("Enter Number of players: "))
+
     def add_players(self):
 
-        if N > 7 or N < 2:
+        if self.N > 7 or self.N < 2:
             print("Enter minimum 2 and maximum 6 players")
-
         else:
-
-            for i in range(N):
+            for i in range(self.N):
                 s = raw_input("Enter Name: ")
-                list1.append(s)
-            list1.sort()
-            print(list1)
+                self.players_list.append(s)
+            self.players_list.sort()
+            print(self.players_list)
 
-class Dice(Player):
+    def initial_score(self, val):
+        for i in range(val):
+            self.score_list.append(0)
+            self.final_score.append(0)
+        return self.score_list, self.final_score
 
-    def Roll(self, score):
+class Dice:
+
+    def __init__(self):
+        self.temp_score = 0
+
+
+    def Roll(self):
+        import random
         score = random.randint(1, 7)
-        if score == 6:
-            score1 = random.randint(1, 7)
-            score = score1+score
-            if score1 == 6:
-                score2 = random.randint(1, 7)
-                score = score2+score
-                if score2 == 6:
-                    score = 0
-    #    score_list[i] = score
+        i = 1
+
+        while score == 6:
+            i =+ 1
+            if i<3:
+                print(score)
+                self.temp_score = self.temp_score+score
+                roll_again = raw_input("Roll again: ")
+                return self.Roll()
+            else:
+                print(score)
+                return self.temp_score
+        else:
+            print(score)
+            score = score+self.temp_score
         return score
 
-    #def Pass(self, pre_score):
-    #    return pre_score
-    #def Exit(self):
-
-    #    pass
 
 
-    def game(self):
-
-
-        for i in range(0, N):
-
-            print("It's your turn: "+list1[i])
-            Opt = raw_input("Roll Exit Pass")
+class game(Dice, Player):
+    mydict = {}
+    def game_play(self):
+        for i in range(x.N):
+            print("It's your turn: "+x.players_list[i])
+            Opt = raw_input("Roll Exit Pass : ")
             if Opt == "Roll":
-                score_list[i] = self.Roll(score)
-                print(score_list[i])
-                print({list1[i] : score_list[i]})
-                final_score[i] = score_list[i] + final_score[i]
-                print(final_score)
-
+                x.score_list[i] = w.Roll()
+                print(x.score_list[i])
+                x.final_score[i] = x.score_list[i] + x.final_score[i]
+                print(x.final_score)
             elif Opt == "Exit":
-                print(score_list[i])
+                print(x.score_list[i])
                 break
-
             elif Opt == "Pass":
-                print(score_list[i])
-
-            #elif final_score[i-1] >= 50:
-
-            #    print("Game over")
-            #    break
+                print(x.score_list[i])
             else:
                 print("Enter valid option")
 
-w = Dice()
-w.add_players()
+    def game_winner(self):
+        sorted_score = x.final_score
+        while sorted_score[-1] < 50:
+            sorted_score.sort()
+            self.game_play()
 
+        for i in range(x.N):
+            self.mydict[x.players_list[i]] = x.final_score[i]
+            print(self.mydict)
+        for a, y in sorted(self.mydict.items(), key=lambda item: item[1]):
+            print("%s: %s" %(a, y))
+        q = sorted(self.mydict.items(), key=lambda item: item[1])
+        print(q)
+        print("1st winner is: {0}".format(q[-1]))
+        print("2st winner is:{0}".format(q[-2]))
 
-if count == 0:
-    for x in range(0, N):
-        score_list.append(0)
-        final_score.append(0)
-    print(score_list)
-    print(final_score)
-    w.game()
-    count = 1
-
-while count == 1:
-    for j in range(N):
-        for q in final_score:
-            while q < 50:
-                w.game()
-
-
-            else:
-                pass
-            #print("First winner is: ")
-        break
+if __name__ == "__main__":
+    w = Dice()
+    x = Player()
+    g = game()
+    x.add_players()                ***To accept and sort list of players ***
+    x.initial_score(x.N)           *** To intialize score list i.e. scores of each round and final score i.e. addition od scores***
+    g.game_winner()                *** To play game and find winner***
