19e3dd6c5bbf1d872955bc29ec5a1463f093ca53
Aseem Shakuntal <ashakunt@users.noreply.github.com>
2019-12-03 14:59:58 +0200

    Example sorting of python object

--- example.py
+++ example.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 import sys
 class Player:
-    def __init__(self, name=None, age=None):
+    def __init__(self, name=None):
         self.name = name or raw_input("Enter name: ")
-        self.age = age or raw_input("Enter age: ")
 
-    def introduce(self):
-        print ("Hello, my name is: %s and I am %d years old" % (self.name, int(self.age)))
+    def introduce(self, count):
+        print ("Hello, my name is: %s (# %d)" % (self.name, count))
 
 class Team:
     def __init__(self):
         self.players = []
         x = raw_input("How many players: ")
         for i in range (0, int(x)):
-            print ("i is = " + str(i))
             p = Player()
             self.players.append(p)
 
-    def show(self):
+    def show(self, msg=None):
         for i in range(0, len(self.players)):
-            self.players[i].introduce()
+            self.players[i].introduce(i+1)
+
+    def sort(self):
+        for i in range(0, len(self.players)):
+            for j in range(i+1, len(self.players)):
+                if self.players[i].name > self.players[j].name:
+                    self.players[i], self.players[j] = self.players[j], self.players[i]
 
 
 if __name__ == '__main__':
     team = Team()
+    team.sort()
     team.show()
